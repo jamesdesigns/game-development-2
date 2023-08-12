b9c5f4bb37efb610a788602aa85b9165d8b286d0
James Hooper <jameshooperdesigns@gmail.com>
2023-08-11 21:38:53 -0600

    Add more items to the script

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ canvas.width = 800;
 canvas.height = 500;
 
 let score = 0;
+let gameFrame = 0;
+ctx.font = '50px Georgia';
 
 // Mouse Interactivity
+const mouse = {
+  x: canvas.width/2,
+  y: canvas.height/2,
+  click: false
+}
+canvas.addEventListener('mousedown', function(event){
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse.x, mouse.y);
+})
 // Player
 // Bubbles
 // Animation Loop
