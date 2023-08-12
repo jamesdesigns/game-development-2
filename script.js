86fe77829ca4ebba2d1ba212ac228451d0058127
James Hooper <jameshooperdesigns@gmail.com>
2023-08-11 21:33:14 -0600

    Add variables to the script

---
+++ script.js
@@ -0,0 +1,12 @@
+// Canvas setup
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+canvas.width = 800;
+canvas.height = 500;
+
+let score = 0;
+
+// Mouse Interactivity
+// Player
+// Bubbles
+// Animation Loop
